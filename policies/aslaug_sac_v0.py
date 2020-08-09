e706c2be5c1c2b1cefe1567aed03be100ecf185b
dennisushi <de1996@mail.bg>
2020-08-09 15:17:32 +0300

    new SAC policy

--- policies/aslaug_sac_v0.py
+++ policies/aslaug_sac_v0.py
@@ -23,7 +23,7 @@ class CustomPolicySAC(SACPolicy):
     :param kwargs: (dict) Extra keyword arguments for the nature CNN feature extraction
     """
     def __init__(self, sess, ob_space, ac_space, n_env=1, n_steps=1, n_batch=None, reuse=False, layers=None,
-        cnn_extractor=nature_cnn, feature_extraction="mlp", reg_weight=0.0,
+        cnn_extractor=nature_cnn, feature_extraction="cnn", reg_weight=0.0,
         layer_norm=False, act_fun=tf.nn.relu, 
         obs_slicing=[0, 6, 9, 57, 64, 71, 272, 473], **kwargs):
         super(CustomPolicySAC, self).__init__(sess, ob_space, ac_space, n_env, n_steps, n_batch,
@@ -33,10 +33,10 @@ class CustomPolicySAC(SACPolicy):
         self.layer_norm = layer_norm
         self.feature_extraction = feature_extraction
         self.cnn_kwargs = kwargs
-        self.cnn_extractor = cnn_extractor
+        self.cnn_extractor = self.make_feature_extractor #cnn_extractor
         self.reuse = reuse
         if layers is None:
-            layers = [64, 32, 16]
+            layers = [256, 128, 64, 32, 16]
         self.layers = layers
         self.reg_loss = None
         self.reg_weight = reg_weight
@@ -51,89 +51,84 @@ class CustomPolicySAC(SACPolicy):
         if obs is None:
             obs = self.processed_obs
         # Scale observation
-        with tf.variable_scope("observation_scaling", reuse=reuse):
-            obs_avg = tf.constant((self.ob_space.high+self.ob_space.low)/2.0,
-                                  name="obs_avg")
-            obs_dif = tf.constant((self.ob_space.high - self.ob_space.low),
-                                  name="obs_diff")
-            shifted = tf.math.subtract(self.processed_obs, obs_avg)
-            proc_obs = tf.math.divide(shifted, obs_dif)
+        obs_avg = tf.constant((self.ob_space.high+self.ob_space.low)/2.0,
+                              name="obs_avg")
+        obs_dif = tf.constant((self.ob_space.high - self.ob_space.low),
+                              name="obs_diff")
+        shifted = tf.math.subtract(self.processed_obs, obs_avg)
+        proc_obs = tf.math.divide(shifted, obs_dif)
         if self.obs_slicing is not None:
             obs_slicing = self.obs_slicing
         else:
             obs_slicing = [0, 6, 9, 57, 64, 71, 272, 473]
         # Create network
-        with tf.variable_scope("model/inputs", reuse=reuse):
-            lrelu = tf.nn.leaky_relu
-            o = obs_slicing
-            in_sp = self.crop(1, o[0], o[1])(proc_obs)
-            in_mb = self.crop(1, o[1], o[2])(proc_obs)
-            in_lp = self.crop(1, o[2], o[3])(proc_obs)
-            in_jp = self.crop(1, o[3], o[4])(proc_obs)
-            in_jv = self.crop(1, o[4], o[5])(proc_obs)
-            in_sc1 = self.crop(1, o[5], o[6])(proc_obs)
-            in_sc2 = self.crop(1, o[6], o[7])(proc_obs)
+        lrelu = tf.nn.leaky_relu
+        o = obs_slicing
+        in_sp = self.crop(1, o[0], o[1])(proc_obs)
+        in_mb = self.crop(1, o[1], o[2])(proc_obs)
+        in_lp = self.crop(1, o[2], o[3])(proc_obs)
+        in_jp = self.crop(1, o[3], o[4])(proc_obs)
+        in_jv = self.crop(1, o[4], o[5])(proc_obs)
+        in_sc1 = self.crop(1, o[5], o[6])(proc_obs)
+        in_sc2 = self.crop(1, o[6], o[7])(proc_obs)
 
-        with tf.variable_scope("model/scan_block", reuse=reuse):
+        sl_1 = tf.layers.Conv1D(2, 11, activation=lrelu, name="sc_1")
+        sl_2 = tf.layers.Conv1D(4, 7, activation=lrelu, name="sc_2")
+        sl_3 = tf.layers.Conv1D(8, 3, activation=lrelu, name="sc_3")
+        sl_4 = tf.layers.MaxPooling1D(3, 3, name="sc_4")
+        sl_5 = tf.layers.Conv1D(8, 7, activation=lrelu, name="sc_5")
+        sl_6 = tf.layers.Conv1D(8, 5, activation=lrelu, name="sc_6")
+        sl_7 = tf.layers.MaxPooling1D(3, 3, name="sc_7")
+        sl_8 = tf.layers.Conv1D(8, 3, activation=lrelu, name="sc_8")
+        sl_9 = tf.layers.Flatten(name="sc_9")
+        sl_10 = tf.layers.Dense(128, activation=lrelu, name="sc_10")
+        sl_11 = tf.layers.Dense(64, activation=lrelu, name="s1_11")
+        sl_out = tf.layers.Dense(64, activation=lrelu, name="s1_out")
 
-            sl_1 = tf.layers.Conv1D(2, 11, activation=lrelu, name="sc_1")
-            sl_2 = tf.layers.Conv1D(4, 7, activation=lrelu, name="sc_2")
-            sl_3 = tf.layers.Conv1D(8, 3, activation=lrelu, name="sc_3")
-            sl_4 = tf.layers.MaxPooling1D(3, 3, name="sc_4")
-            sl_5 = tf.layers.Conv1D(8, 7, activation=lrelu, name="sc_5")
-            sl_6 = tf.layers.Conv1D(8, 5, activation=lrelu, name="sc_6")
-            sl_7 = tf.layers.MaxPooling1D(3, 3, name="sc_7")
-            sl_8 = tf.layers.Conv1D(8, 3, activation=lrelu, name="sc_8")
-            sl_9 = tf.layers.Flatten(name="sc_9")
-            sl_10 = tf.layers.Dense(128, activation=lrelu, name="sc_10")
-            sl_11 = tf.layers.Dense(64, activation=lrelu, name="s1_11")
-            sl_out = tf.layers.Dense(64, activation=lrelu, name="s1_out")
+        s1_0 = tf.expand_dims(in_sc1, -1)
+        s1_1 = sl_1(s1_0)
+        s1_2 = sl_2(s1_1)
+        s1_3 = sl_3(s1_2)
+        s1_4 = sl_4(s1_3)
+        s1_5 = sl_5(s1_4)
+        s1_6 = sl_6(s1_5)
+        s1_7 = sl_7(s1_6)
+        s1_8 = sl_8(s1_7)
+        s1_9 = sl_9(s1_8)
+        s1_10 = sl_10(s1_9)
+        s1_11 = sl_11(s1_10)
+        s1_out = sl_out(s1_11)
 
-            s1_0 = tf.expand_dims(in_sc1, -1)
-            s1_1 = sl_1(s1_0)
-            s1_2 = sl_2(s1_1)
-            s1_3 = sl_3(s1_2)
-            s1_4 = sl_4(s1_3)
-            s1_5 = sl_5(s1_4)
-            s1_6 = sl_6(s1_5)
-            s1_7 = sl_7(s1_6)
-            s1_8 = sl_8(s1_7)
-            s1_9 = sl_9(s1_8)
-            s1_10 = sl_10(s1_9)
-            s1_11 = sl_11(s1_10)
-            s1_out = sl_out(s1_11)
+        s2_0 = tf.expand_dims(in_sc2, -1)
+        s2_1 = sl_1(s2_0)
+        s2_2 = sl_2(s2_1)
+        s2_3 = sl_3(s2_2)
+        s2_4 = sl_4(s2_3)
+        s2_5 = sl_5(s2_4)
+        s2_6 = sl_6(s2_5)
+        s2_7 = sl_7(s2_6)
+        s2_8 = sl_8(s2_7)
+        s2_9 = sl_9(s2_8)
+        s2_10 = sl_10(s2_9)
+        s2_11 = sl_11(s2_10)
+        s2_out = sl_out(s2_11)
 
-            s2_0 = tf.expand_dims(in_sc2, -1)
-            s2_1 = sl_1(s2_0)
-            s2_2 = sl_2(s2_1)
-            s2_3 = sl_3(s2_2)
-            s2_4 = sl_4(s2_3)
-            s2_5 = sl_5(s2_4)
-            s2_6 = sl_6(s2_5)
-            s2_7 = sl_7(s2_6)
-            s2_8 = sl_8(s2_7)
-            s2_9 = sl_9(s2_8)
-            s2_10 = sl_10(s2_9)
-            s2_11 = sl_11(s2_10)
-            s2_out = sl_out(s2_11)
+        sc_0 = tf.keras.layers.Concatenate(name="sc_0")([s1_out, s2_out])
+        sc_1 = tf.layers.Dense(128, activation=lrelu, name="sc_1")(sc_0)
+        sc_2 = tf.layers.Dense(64, activation=lrelu, name="sc_2")(sc_1)
+        sc_out = tf.layers.Dense(64, activation=lrelu, name="sc_out")(sc_2)
 
-            sc_0 = tf.keras.layers.Concatenate(name="sc_0")([s1_out, s2_out])
-            sc_1 = tf.layers.Dense(128, activation=lrelu, name="sc_1")(sc_0)
-            sc_2 = tf.layers.Dense(64, activation=lrelu, name="sc_2")(sc_1)
-            sc_out = tf.layers.Dense(64, activation=lrelu, name="sc_out")(sc_2)
-
-        with tf.variable_scope("model/combination_block", reuse=reuse):
-            c_0 = tf.keras.layers.Concatenate(name="c_0")([sc_out, in_sp,
-                                                           in_mb, in_lp, in_jp,
-                                                           in_jv])
-            c_1 = tf.layers.Dense(512, activation=lrelu, name="c_1")(c_0)
-            c_2 = tf.layers.Dense(256, activation=lrelu, name="c_2")(c_1)
-            c_3 = tf.layers.Dense(256, activation=lrelu, name="c_3")(c_2)
-            c_4 = tf.layers.Dense(128, activation=lrelu, name="c_4")(c_3)
-            c_5 = tf.layers.Dense(128, activation=lrelu, name="c_5")(c_4)
-            c_6 = tf.layers.Dense(64, activation=lrelu, name="c_6")(c_5)
-            c_7 = tf.layers.Dense(64, activation=lrelu, name="c_7")(c_6)
-            c_out = tf.layers.Dense(32, activation=lrelu, name="c_out")(c_7)
+        c_0 = tf.keras.layers.Concatenate(name="c_0")([sc_out, in_sp,
+                                                       in_mb, in_lp, in_jp,
+                                                       in_jv])
+        c_1 = tf.layers.Dense(512, activation=lrelu, name="c_1")(c_0)
+        c_2 = tf.layers.Dense(256, activation=lrelu, name="c_2")(c_1)
+        c_3 = tf.layers.Dense(256, activation=lrelu, name="c_3")(c_2)
+        c_4 = tf.layers.Dense(128, activation=lrelu, name="c_4")(c_3)
+        c_5 = tf.layers.Dense(128, activation=lrelu, name="c_5")(c_4)
+        c_6 = tf.layers.Dense(64, activation=lrelu, name="c_6")(c_5)
+        c_7 = tf.layers.Dense(64, activation=lrelu, name="c_7")(c_6)
+        c_out = tf.layers.Dense(32, activation=lrelu, name="c_out")(c_7)
 
         feature_extractor = c_out
         return feature_extractor
@@ -141,8 +136,6 @@ class CustomPolicySAC(SACPolicy):
     def make_actor(self, obs=None, reuse=False, scope="pi"):
         if obs is None:
             obs = self.processed_obs
-
-        obs = self.make_feature_extractor(obs, reuse=tf.AUTO_REUSE)
 
         with tf.variable_scope(scope, reuse=reuse):
             if self.feature_extraction == "cnn":
@@ -185,8 +178,6 @@ class CustomPolicySAC(SACPolicy):
                      create_vf=True, create_qf=True):
         if obs is None:
             obs = self.processed_obs
-
-        obs = self.make_feature_extractor(obs, reuse=tf.AUTO_REUSE)
 
         with tf.variable_scope(scope, reuse=reuse):
             if self.feature_extraction == "cnn":
